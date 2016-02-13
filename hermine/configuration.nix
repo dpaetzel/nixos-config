70a0ba10f4aac4f758e4d16f6818d9b8c901e266
David Hoffmann <david.and.hoffmann+github@gmail.com>
2016-02-13 10:14:44 +0100

    browser stuff, e.g. flash

--- hermine/configuration.nix
+++ hermine/configuration.nix
@@ -85,6 +85,20 @@
     # bc
   ];
 
+  nixpkgs.config = {
+    allowUnfree = true;
+
+    firefox = {
+      enableGoogleTalkPlugin = true;
+      enableAdobeFlash = true;
+    };
+
+    chromium = {
+      enablePepperFlash = true; # Chromium removed support for Mozilla (NPAPI) plugins so Adobe Flash no longer works
+      enablePepperPDF = true;
+    };
+  };
+
   # Enable pulseaudio.
   hardware.pulseaudio.enable = true;
 
