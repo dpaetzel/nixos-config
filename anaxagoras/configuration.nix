f9c06883a65f7e60cd1ac8d962c869ff1a3a3dca
David Pätzel <david.a.paetzel+github@gmail.com>
2016-11-05 18:35:39 +0100

    remove fruitless hostname check

--- anaxagoras/configuration.nix
+++ anaxagoras/configuration.nix
@@ -80,10 +80,8 @@
     videoDrivers = [ "nouveau" ];
     displayManager.sessionCommands =
       ''
-      if [[ $(hostname) == anaxagoras ]]; then
-          sleep 1
-          xrandr --output DP-1 --off --output DVI-I-1 --mode 1280x1024 --pos 0x400 --rotate left --output DVI-D-1 --mode 1680x1050 --pos 2944x0 --rotate right --output HDMI-1 --mode 1920x1080 --pos 1024x400 --rotate normal --primary
-      fi
+        sleep 1
+        xrandr --output DP-1 --off --output DVI-I-1 --mode 1280x1024 --pos 0x400 --rotate left --output DVI-D-1 --mode 1680x1050 --pos 2944x0 --rotate right --output HDMI-1 --mode 1920x1080 --pos 1024x400 --rotate normal --primary
       '';
 
     displayManager.slim.defaultUser = "david";
