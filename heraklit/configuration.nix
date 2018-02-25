e0fb96ba48450b9b77c8aec87402a350951e495e
David Pätzel <david.a.paetzel@gmail.com>
2018-02-25 16:48:56 +0100

    Add new media station configuration for heraklit

--- heraklit/configuration.nix
+++ heraklit/configuration.nix
@@ -4,9 +4,6 @@
   imports =
     [
       ../common.nix
-      # ../desktop.nix
-      ../theme.nix
-      # ../workstation.nix
       ../media.nix
     ];
 
@@ -31,35 +28,18 @@
     ];
   };
 
-  # the encrypted partition
-  boot.initrd.luks.devices = [
-    { name = "crypted";
-      device = "/dev/sda3";
-      preLVM = true;
-    }
-  ];
-
   fileSystems."/" =
-    # { device = "/dev/disk/by-uuid/c9e1e74d-b05b-4e22-a896-28ac43566d04";
-    { device = "/dev/vg/root";
+    { device = "/dev/disk/by-uuid/4f351262-a1cd-443c-a14d-37aca4336eb1";
       fsType = "ext4";
     };
 
-  fileSystems."/boot" =
-    # { device = "/dev/disk/by-uuid/89623a96-8cbb-4740-a387-7d3896d95768";
-    { device = "/dev/disk/by-label/boot";
-      fsType = "ext2";
-    };
-
   fileSystems."/home" =
-    # { device = "/dev/disk/by-uuid/1cf62dd4-ed52-4f0a-a44f-ed3fa28f9d50";
-    { device = "/dev/vg/home";
+    { device = "/dev/disk/by-uuid/f34b28fa-6ebd-4b8e-9261-4ac6fc85c19e";
       fsType = "ext4";
     };
 
   swapDevices =
-    # [ { device = "/dev/disk/by-uuid/73e49284-5397-4a58-95f0-1f71f6d4002b"; }
-    [ { device = "/dev/vg/swap"; }
+    [ { device = "/dev/disk/by-uuid/ff4fa9fc-adc1-48a4-b21f-ff418ab338d8"; }
     ];
 
   # use the GRUB 2 boot loader
@@ -74,27 +54,21 @@
     "ahci"
     "xhci_pci"
     "usb_storage"
-    "usbhid"
+    "sd_mod"
+    "sr_mod"
+    "sdhci_pci"
+    "rtsx_pci_sdmmc"
   ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
+  boot.kernelParams = [ "video=LVDS-1:d" ];
 
   services.xserver.videoDrivers = lib.mkForce [ "intel" ];
-
-  # seems to make stuff like Chromium go slightly bananas in terms of performance
-  # hardware.opengl.extraPackages = with pkgs; [
-  #   vaapiIntel libvdpau-va-gl vaapiVdpau
-  # ];
 
   # other services
   services.openssh.enable = true;
   services.tlp.enable = true; # power management/saving for laptops
   services.cron.enable = true;
-  # udev rule for my android phone
-  services.udev.extraRules = ''
-    SUBSYSTEM=="usb", ATTR{idVendor}=="18d1", MODE="0666"
-    SUBSYSTEM=="usb", ATTR{idVendor}=="054c", MODE="0666"
-  '';
 
   # “A list of files containing trusted root certificates in PEM format. These
   # are concatenated to form /etc/ssl/certs/ca-certificates.crt”
@@ -106,22 +80,6 @@
   # each and hyper-threading).”
   nix.maxJobs = 4;
 
-  networking.networkmanager.basePackages =
-    with pkgs; {
-      # needed for university vpn; thanks Profpatsch!
-      networkmanager_openconnect =
-        pkgs.networkmanager_openconnect.override { openconnect = pkgs.openconnect_gnutls; };
-      inherit networkmanager modemmanager wpa_supplicant
-              networkmanager_openvpn networkmanager_vpnc
-              networkmanager_pptp networkmanager_l2tp;
-  };
-  # networking.wireless.enable = true;  # wireless support via wpa_supplicant
-
-  services.printing = {
-    enable = true;
-    drivers = [ pkgs.gutenprint ];
-  };
-
   nixpkgs.config = {
     kodi = {
       enableAdvancedLauncher = true;
@@ -131,26 +89,14 @@
   environment.systemPackages =
     with (import ../packages.nix pkgs);
       system ++
-      applications.main ++
-      applications.utility ++
-      graphical-user-interface ++
-      mutt ++
       commandline.main ++
-      commandline.utility
-      ++
       [
-      # kodi
-      # pkgs.kodiPlugins.advanced-launcher
-      pkgs.matchbox
-      pkgs.jwm
-      ]
-      # ++
-      # development
-      # ++
-      # (with pkgs; [
-      #   (with texlive; combine {
-      #     inherit scheme-full; # wrapfig capt-of biblatex biblatex-ieee logreq xstring newtx;
-      #   })
-      # ]);
-      ;
+        pkgs.pavucontrol
+        pkgs.vim
+        pkgs.google-chrome
+        # kodi
+        # pkgs.kodiPlugins.advanced-launcher
+        pkgs.matchbox
+        pkgs.jwm
+      ];
 }
