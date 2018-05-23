0f454c4e6d772e95d884d5d55593e3ca43a1b918
David Pätzel <david.a.paetzel+github@gmail.com>
2018-05-23 15:01:55 +0200

    Fix system.nixos.stateVersion attribute name

--- common.nix
+++ common.nix
@@ -15,7 +15,7 @@
 
 {
   # The NixOS release to be compatible with for stateful data such as databases.
-  system.stateVersion = "18.03";
+  system.nixos.stateVersion = "18.03";
 
   # “Turn on this option if you want to enable all the firmware shipped in
   # linux-firmware.”
