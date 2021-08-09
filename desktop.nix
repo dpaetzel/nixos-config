c026a5d088b6a729b74b83091d5421a0205ea4f0
David Pätzel <david.a.paetzel+github@gmail.com>
2021-08-09 09:36:55 +0200

    Fix screen tearing (e.g. in Chrome)

--- desktop.nix
+++ desktop.nix
@@ -28,8 +28,12 @@
 
   services.compton = {
     enable = true;
+    # 2021-08-09 Solves screen tearing in Chrome for me.
+    # https://www.reddit.com/r/archlinux/comments/j58isj/i3gaps_picom_screen_tearing_issue/
+    backend = "glx";
     fade = true;
     inactiveOpacity = 0.7;
     opacityRules = [ "100:class_g = 'dmenu'" ];
+    vSync = true;
   };
 }
