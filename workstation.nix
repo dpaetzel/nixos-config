54b3b7e768d926b6433f69a4c23bd478fb17a327
David Pätzel <david.a.paetzel+github@gmail.com>
2022-01-21 14:44:03 +0100

    Disable virtualbox to save space

--- workstation.nix
+++ workstation.nix
@@ -3,7 +3,7 @@
 {
   virtualisation.docker.enable = true;
   virtualisation.lxd.enable = true;
-  virtualisation.virtualbox.host.enable = true;
-  virtualisation.virtualbox.host.enableExtensionPack = true;
+  # virtualisation.virtualbox.host.enable = true;
+  # virtualisation.virtualbox.host.enableExtensionPack = true;
 }
 
