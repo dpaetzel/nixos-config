5f9ca93b008662a0769f6946eddfbc9e47fb5407
David Pätzel <david.a.paetzel+github@gmail.com>
2023-02-06 14:40:45 +0100

    🐛 Fix srid cachix public key

--- sokrates/cachix/srid.nix
+++ sokrates/cachix/srid.nix
@@ -4,7 +4,7 @@
       "https://srid.cachix.org"
     ];
     trusted-public-keys = [
-      "srid.cachix.org-1:MTQ6ksbfz3LBMmjyPh0PLmos+1x+CdtJxA/J2W+PQxI="
+      "srid.cachix.org-1:3clnql5gjbJNEvhA/WQp7nrZlBptwpXnUk6JAv8aB2M="
     ];
   };
 }
