893c561879e70a154f2d46f4d08311396dc4586f
David Pätzel <david.a.paetzel+github@gmail.com>
2023-05-24 10:06:53 +0200

    ✨ Enable libvirtd

--- workstation.nix
+++ workstation.nix
@@ -3,6 +3,7 @@
 {
   # virtualisation.docker.enable = true;
   # virtualisation.lxd.enable = true;
+  virtualisation.libvirtd.enable = true;
   virtualisation.virtualbox.host.enable = true;
   virtualisation.virtualbox.host.enableExtensionPack = true;
 }
