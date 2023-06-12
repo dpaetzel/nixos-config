fa9a67a63c59c1a7c784c380c570d18911fa09be
David Pätzel <david.a.paetzel+github@gmail.com>
2023-06-12 15:08:19 +0200

    ⏪ Re-enable docker

--- workstation.nix
+++ workstation.nix
@@ -1,7 +1,7 @@
 { ... }:
 
 {
-  # virtualisation.docker.enable = true;
+  virtualisation.docker.enable = true;
   # virtualisation.lxd.enable = true;
   virtualisation.libvirtd.enable = true;
   virtualisation.virtualbox.host.enable = true;
