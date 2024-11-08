8a582f34977b2426380aa4a8aada66e809c872a3
David Pätzel <david.paetzel@posteo.de>
2024-11-08 16:21:22 +0100

    ✨ Make `editor` sync by default
    You can recover the async behaviour by doing `editor --no-wait …`.

--- scripts/editor.nix
+++ scripts/editor.nix
@@ -1,7 +1,10 @@
 { writeShellScriptBin, myemacs }:
 
+# Note: I removed `--no-wait` to have the option of doing `emacsclient`
+# synchronously. This means that all apps using `editor` have to add `--no-wait`
+# if they require async.
 (writeShellScriptBin "editor" ''
   pgrep emacs \
-    && ${myemacs}/bin/emacsclient --create-frame --no-wait "$@" \
+    && ${myemacs}/bin/emacsclient --create-frame "$@" \
     || ${myemacs}/bin/emacs --tty "$@"
 '')
