b43d33dc9764b0a86850e2dc94f47cdb679d9fec
David Pätzel <david.paetzel@posteo.de>
2025-01-03 15:15:55 +0100

    🐛 Fix MuseScore by using the AppImage directly

--- flake.nix
+++ flake.nix
@@ -69,6 +69,30 @@
           # (self: super: {
           #   steam = super.steam.override { extraPkgs = pkgs: with pkgs; [ super.openssl ]; };
           # })
+          # MuseScore is broken but the AppImage isn't, from
+          # https://github.com/NixOS/nixpkgs/issues/343456#issuecomment-2568942308
+          (final: prev: {
+            musescore =
+              let
+                pname = "musescore-appimage";
+                version = "4.4.4";
+                src = prev.fetchurl {
+                  url = "https://cdn.jsdelivr.net/musescore/v${version}/MuseScore-Studio-${version}.243461245-x86_64.AppImage";
+                  hash = "sha256-g5mb9mPqh5lDV2wIBugzFMKtjJzGuXm5mIZVvsyRBh4=";
+                };
+                appimageContents = prev.appimageTools.extractType2 { inherit pname version src; };
+              in
+              prev.appimageTools.wrapType2 {
+                inherit pname version src;
+
+                extraInstallCommands = ''
+                  install -Dm444 ${appimageContents}/share/applications/org.musescore.MuseScore4portable.desktop -t $out/share/applications
+                  substituteInPlace $out/share/applications/org.musescore.MuseScore4portable.desktop \
+                    --replace-fail 'Exec=mscore4portable %U' 'Exec=${pname}'
+                  cp -r ${appimageContents}/share/icons $out/share
+                '';
+              };
+          })
         ];
       };
 
