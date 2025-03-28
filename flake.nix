9bfe64bbd25addd100c9675a0c5ca94fe72342ca
David Pätzel <david.paetzel@posteo.de>
2025-03-28 19:00:14 +0100

    Disable unnecessary nixpkgs.follows

--- flake.nix
+++ flake.nix
@@ -20,10 +20,8 @@
     musnix.inputs.nixpkgs.follows = "nixpkgs";
 
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
-    nixos-hardware.inputs.nixpkgs.follows = "nixpkgs";
 
     overlays.url = "github:dpaetzel/overlays/master";
-    overlays.inputs.nixpkgs.follows = "nixpkgs";
 
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
